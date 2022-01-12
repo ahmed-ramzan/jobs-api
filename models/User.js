7b20c57cc3a0a3657abe03049f7805dd1fdbfad3
ahmed-ramzan <ahmadramzan562@gmail.com>
2022-01-12 07:49:14 +0500

    login-user

--- models/User.js
+++ models/User.js
@@ -19,4 +19,9 @@ userSchema.methods.createJWT = function () {
     return jwt.sign({ userId: this._id, name: this.name }, process.env.JWT_SECRET, {expiresIn: process.env.JWT_LIFETIME})
 }
 
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    const isMatch = await bcrypt.compare(candidatePassword, this.password)
+    return isMatch
+}
+
 module.exports = mongoose.model('User', userSchema)
